c0f6bb342111f18ce1264da8f0cada203116e8de
My Name <myemail@example.com>
2025-02-26 09:35:19 +0800

    adding simple get status

--- src/server.mjs
+++ src/server.mjs
@@ -6,6 +6,15 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+//get
+app.get("/api/users", (req, res) => {
+  res.status(201).sendFile({ msg: "hello" });
+});
+
+app.get("api/products", (request, response) => {
+  response.send([{ name: "Rico", lname: "Alentio" }]);
+});
+
 app.listen(PORT, () => {
   console.log(`Running port of ${PORT}`);
 });
