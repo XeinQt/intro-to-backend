67a0969d4af9f1bb59901f5a9bcf8dfe1c97c068
My Name <myemail@example.com>
2025-02-26 10:03:43 +0800

    adding with a access questy for filtering

--- src/server.mjs
+++ src/server.mjs
@@ -9,6 +9,14 @@ const PORT = process.env.PORT;
 const users = [
   { id: 1, name: "ico", lname: "alentio" },
   { id: 2, name: "htodog", lname: "haha" },
+  { id: 3, name: "john", lname: "doe" },
+  { id: 4, name: "jane", lname: "smith" },
+  { id: 5, name: "mike", lname: "tyson" },
+  { id: 6, name: "lisa", lname: "johnson" },
+  { id: 7, name: "dave", lname: "brown" },
+  { id: 8, name: "sara", lname: "white" },
+  { id: 9, name: "alex", lname: "miller" },
+  { id: 10, name: "chris", lname: "wilson" },
 ];
 
 //get
@@ -29,6 +37,21 @@ app.get("/api/users/:id", (request, response) => {
   const findUser = users.find((user) => user.id === parseId);
 
   return response(findUser);
+});
+
+//reaquest a quety
+app.get("/api/users", (request, response) => {
+  const { name } = request.query;
+
+  if (name) {
+    const filteredUsers = users.filter(
+      (user) => user.name.toLowerCase() === name.toLowerCase()
+    );
+
+    return response.json(users);
+  }
+
+  response.json(users);
 });
 
 app.listen(PORT, () => {
