6b342de76e30afaa0494b1384960d8008533cbc7
My Name <myemail@example.com>
2025-02-26 09:48:30 +0800

    adding with a access with params

--- src/server.mjs
+++ src/server.mjs
@@ -1,10 +1,15 @@
 import express from "express";
-import dotenv from "dotenv";
+import dotenv, { parse } from "dotenv";
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
+
+const users = [
+  { id: 1, name: "ico", lname: "alentio" },
+  { id: 2, name: "htodog", lname: "haha" },
+];
 
 //get
 app.get("/api/users", (req, res) => {
@@ -13,6 +18,17 @@ app.get("/api/users", (req, res) => {
 
 app.get("api/products", (request, response) => {
   response.send([{ name: "Rico", lname: "Alentio" }]);
+});
+
+//acces with params
+app.get("/api/users/:id", (request, response) => {
+  console.log(request.params.id);
+
+  const parseId = parseInt(request.params.id);
+  if (isNaN(parseId)) return response.status(400).send([{ msg: "error" }]);
+  const findUser = users.find((user) => user.id === parseId);
+
+  return response(findUser);
 });
 
 app.listen(PORT, () => {
